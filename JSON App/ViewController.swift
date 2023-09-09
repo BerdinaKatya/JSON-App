456aa6e88dd9f738839dd700e881bf6a24dc10da
BerdinaKatya <kuznkate42@gmail.com>
2023-09-09 08:24:01 +0500

    Initial Commit

---
+++ JSON App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  JSON App
+//
+//  Created by Екатерина Теляубердина on 09.09.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
